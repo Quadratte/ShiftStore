64662b50641e729aa5df35b6ee786650d2cd6ade
Sergey Philippov <Quadratte@gmail.com>
2025-08-04 23:52:46 +0700

    Add login view elements and constraints

--- ShiftStore/Controllers/ViewController.swift
+++ ShiftStore/Controllers/ViewController.swift
@@ -8,51 +8,90 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+  
   lazy var userName: UITextField = {
     let userName = UITextField()
-    userName.translatesAutoresizingMaskIntoConstraints = false
-    userName.delegate = self
-    userName.backgroundColor = .white
-    userName.layer.cornerRadius = 6
-    userName.layer.borderWidth = 2
-    userName.layer.borderColor = UIColor.blue.cgColor
-    userName.placeholder = "Enter your name"
-    return userName
+    return userName.registerTextField(placeholder: "Имя", isSecureEntry: false)
   }()
   
-  lazy var dateOfBurth: UIDatePicker = {
+  lazy var userLastName: UITextField = {
+    let userLastName = UITextField()
+    return userLastName.registerTextField(placeholder: "Фамилия", isSecureEntry: false)
+  }()
+  
+  lazy var userPassword: UITextField = {
+    let userPassword = UITextField()
+    return userPassword.registerTextField(placeholder: "Пароль", isSecureEntry: true)
+  }()
+  
+  lazy var confirmUserPassword: UITextField = {
+    let confurmPassword = UITextField()
+    return confurmPassword.registerTextField(placeholder: "Подтвердите пароль", isSecureEntry: true)
+  }()
+  
+  lazy var dateOfBirth: UIDatePicker = {
     let datePicker = UIDatePicker()
     datePicker.translatesAutoresizingMaskIntoConstraints = false
     datePicker.datePickerMode = .date
     datePicker.layer.cornerRadius = 6
     datePicker.layer.masksToBounds = true
     datePicker.backgroundColor = .white
-  
     datePicker.tintColor = .blue
     return datePicker
+  }()
+  
+  lazy var mainButton: UIButton = {
+    let button = UIButton()
+    return button.mainButton(title: "Регистрация")
   }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
-    view.addSubview(userName)
-    view.addSubview(dateOfBurth)
+    setupUI()
     setupConstraints()
-    
+  }
+
+  private func setupUI() {
+    view.addSubview(userName)
+    view.addSubview(userLastName)
+    view.addSubview(dateOfBirth)
+    view.addSubview(userPassword)
+    view.addSubview(confirmUserPassword)
+    view.addSubview(mainButton)
   }
   
   private func setupConstraints() {
     
     NSLayoutConstraint.activate([
-      userName.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+      
+      userName.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
       userName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
       userName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-      userName.heightAnchor.constraint(equalToConstant: 32),
+      userName.heightAnchor.constraint(equalToConstant: 42),
       
-      dateOfBurth.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 20),
-      dateOfBurth.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-      //dateOfBurth.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      userLastName.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 20),
+      userLastName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      userLastName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      userLastName.heightAnchor.constraint(equalToConstant: 42),
+      
+      dateOfBirth.topAnchor.constraint(equalTo: userLastName.bottomAnchor, constant: 20),
+      dateOfBirth.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      
+      userPassword.topAnchor.constraint(equalTo: dateOfBirth.bottomAnchor, constant: 20),
+      userPassword.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      userPassword.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      userPassword.heightAnchor.constraint(equalToConstant: 42),
+      
+      confirmUserPassword.topAnchor.constraint(equalTo: userPassword.bottomAnchor, constant: 20),
+      confirmUserPassword.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      confirmUserPassword.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      confirmUserPassword.heightAnchor.constraint(equalToConstant: 42),
+      
+      mainButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      mainButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+      mainButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+      mainButton.heightAnchor.constraint(equalToConstant: 48),
     ])
   }
 }
@@ -60,4 +99,7 @@ final class ViewController: UIViewController {
 
 extension ViewController: UITextFieldDelegate {
   
+  
+  
 }
+
---
+++ ShiftStore/Utils/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  ShiftStore
+//
+//  Created by S&Q on 04.08.2025.
+//
+
+import UIKit
+
+extension UITextField {
+  
+  func registerTextField(placeholder: String, isSecureEntry: Bool) -> UITextField {
+    let textField = UITextField()
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    textField.backgroundColor = .white
+    textField.layer.cornerRadius = 6
+    textField.layer.borderWidth = 2
+    textField.layer.borderColor = UIColor.blue.cgColor
+    textField.leftViewMode = .always
+    textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 42))
+    textField.placeholder = placeholder
+    textField.isSecureTextEntry = isSecureEntry
+    return textField
+  }
+}
+
+extension UIButton {
+  
+  func mainButton(title: String) -> UIButton {
+    let button = UIButton()
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle(title, for: .normal)
+    button.backgroundColor = .blue
+    button.layer.cornerRadius = 12
+    return button
+  }
+}

6b6c080cdadd9f40f94766a21cfef5d9260ef83d
Sergey Philippov <Quadratte@gmail.com>
2025-08-08 00:21:48 +0700

    Add NetworkLayer and TableData class

--- ShiftStore/Controllers/MainViewController.swift
+++ ShiftStore/Controllers/MainViewController.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-  
-  let data = MockTableData.getData()
-  
+
+  private var tableData: [TableData] = []
+  private let networkLayer = NetworkLayer.shared
+
   let tableView: UITableView = {
     let tableView = UITableView()
     tableView.translatesAutoresizingMaskIntoConstraints = false
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     return tableView
   }()
-  
+
   let greetingButton: UIButton = {
     let button = UIButton()
     button.translatesAutoresizingMaskIntoConstraints = false
@@ -26,14 +27,15 @@ final class MainViewController: UIViewController {
     button.layer.cornerRadius = 12
     return button
   }()
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     setupUI()
     setupConstraints()
     setupActions()
+    loadData()
   }
-  
+
   private func setupUI() {
     tableView.dataSource = self
     navigationItem.title = "Главный экран"
@@ -41,52 +43,81 @@ final class MainViewController: UIViewController {
     view.addSubview(tableView)
     view.addSubview(greetingButton)
   }
-  
+
   private func setupConstraints() {
     NSLayoutConstraint.activate([
       tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
       tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
       tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
       tableView.bottomAnchor.constraint(equalTo: greetingButton.topAnchor, constant: -20),
-      
+
       greetingButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
       greetingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-      greetingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+      greetingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -20),
       greetingButton.heightAnchor.constraint(equalToConstant: 48),
-      
+
     ])
   }
   
-  private func setupActions() {
-    greetingButton.addTarget(self, action: #selector(greetingButtonTapped), for: .touchUpInside)
+  private func loadData() {
+    
+    networkLayer.fetchProducts { [weak self] result in
+      DispatchQueue.main.async {
+        
+        switch result {
+        case .success(let products):
+          self?.tableData = products
+          self?.tableView.reloadData()
+        case .failure(let error):
+          print(error.localizedDescription)
+        }
+      }
+    }
   }
   
+  private func setupActions() {
+    greetingButton.addTarget(
+      self,
+      action: #selector(greetingButtonTapped),
+      for: .touchUpInside
+    )
+  }
+
   @objc private func greetingButtonTapped() {
-    guard let userData = UserDefaults.standard.dictionary(forKey: "userData") as? [String: String],
-          let firstName = userData["firstName"] else {
+    guard
+      let userData = UserDefaults.standard.dictionary(forKey: "userData")
+        as? [String: String],
+      let firstName = userData["firstName"]
+    else {
       print("button did tapped")
       return
     }
-    
+
     let greetingVC = GreetingViewController()
     greetingVC.configureLabel(name: firstName)
     present(greetingVC, animated: true)
   }
-  
 }
 
 extension MainViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    data.count
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+    -> Int
+  {
+    tableData.count
   }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    let item = data[indexPath.row]
-    
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+    -> UITableViewCell
+  {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: "cell",
+      for: indexPath
+    )
+    let item = tableData[indexPath.row]
+
     var config = cell.defaultContentConfiguration()
-    config.text = item.userFirstName
-    config.secondaryText = item.userLastName
+    config.text = item.title
+    config.secondaryText = String(item.price)
     cell.contentConfiguration = config
     return cell
   }
